10ac473950fd7b374b6e056f3e44a6b08047f686
OlgaF <o170385@yandex.com>
2021-11-16 17:44:32 +0100

    changed lamp script

--- Assets/Scripts/LampBroken.cs
+++ Assets/Scripts/LampBroken.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lamp : MonoBehaviour
+public class LampBroken : MonoBehaviour
 {
     Rigidbody2D rb;
     public float moveSpeed;
