8a022b62fd8c2ede800b7b24dc7f1a8e081d7529
OlgaF <o170385@yandex.com>
2021-11-18 17:43:21 +0100

    cables and floor

--- Assets/Scripts/SlipperyFloor.cs
+++ Assets/Scripts/SlipperyFloor.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SlipperyFloor : MonoBehaviour
 {
-    public PlayerMovement player;
+    PlayerMovement player;
     Collider2D floorCollider;
+    PlayerMovement pm;
+    Rigidbody2D prb;
+    GameObject p;
+
     public Vector2 acceleration = new Vector2(50f, 0f);
 
     // Start is called before the first frame update
     void Start()
     {
         floorCollider = GetComponent<Collider2D>();
-        
+        p = GameObject.FindGameObjectWithTag("Player");
+        pm = p.GetComponent<PlayerMovement>();
+        prb = p.GetComponent<Rigidbody2D>();
+        if (p == null) Debug.Log("Player missing, is player tagged?");
+
     }
 
     // Update is called once per frame
@@ -22,7 +30,7 @@ public class SlipperyFloor : MonoBehaviour
     }
     protected void OnCollisionEnter2D(Collision2D collision)
     {
-        player.GetComponent<Rigidbody2D>().velocity = player.GetComponent<Rigidbody2D>().velocity * acceleration*Time.deltaTime; 
+        prb.GetComponent<Rigidbody2D>().velocity = player.GetComponent<Rigidbody2D>().velocity * acceleration*Time.deltaTime; 
     }
     protected void OnCollisionExit2D(Collision2D collision)
     {
