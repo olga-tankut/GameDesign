1748c71165b0277fff12e37ecec1da12b50ecc65
m <Marvine-mail@web.de>
2021-12-15 00:33:50 +0100

    Revert "Merge remote-tracking branch 'origin/lvl-design-2' into scalablePlayer"
    This reverts commit 011a90372f59cd116a00a8955dea187f271c1a26.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,9 +9,6 @@ public class GameManager : MonoBehaviour
    public bool IsGameplayActive = true;
 
    public float restartDelay = 1f;
-   public GameObject pauseScreen;
-   public GameObject gameScreen;
-   public GameObject winScreen;
    private GameObject pauseScreen;
    private GameObject gameScreen;
    private GameObject winScreen;
@@ -27,11 +23,6 @@ public class GameManager : MonoBehaviour
     }
 
     private void Start() {
-       //pauseScreen = GameObject.FindGameObjectWithTag("PauseScreen");
-       //gameScreen = GameObject.FindGameObjectWithTag("GameScreen");
-       pauseScreen.SetActive(false);
-       winScreen.SetActive(false);
-       gameScreen.SetActive(true);
         /*pauseScreen = GameObject.FindGameObjectWithTag("PauseScreen");
         gameScreen = GameObject.FindGameObjectWithTag("GameScreen");
         giveTryScreen = GameObject.FindGameObjectWithTag("GiveTryScreen");
@@ -93,26 +84,8 @@ public class GameManager : MonoBehaviour
         }   
     }
 
-    public void LoadNextLevel() //Load next lvl without a timer, used for buttons
+    public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void LoadNextLevel(float delay, int index) //Load next lvl with a delay + index
-    {
-        winScreen.SetActive(true); //Activate winScreen
-        StartCoroutine(LoadDelay(delay, index)); //Start delay
-    }
-
-    public void LoadNextLevel(float delay) //Load next lvl with a delay + NEXT index
-    {
-        winScreen.SetActive(true); //Activate winScreen
-        StartCoroutine(LoadDelay(delay, SceneManager.GetActiveScene().buildIndex + 1)); //Start delay
-    }
-
-    private IEnumerator LoadDelay(float time, int index) //Scene loader that loads next lvl after delay
-    {
-       yield return new WaitForSeconds(time);
-       SceneManager.LoadScene(index);
     }
 }
--- Assets/Scripts/LevelLoadTrigger.cs
+++ Assets/Scripts/LevelLoadTrigger.cs
@@ -5,46 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoadTrigger : MonoBehaviour
 {
-    public bool disableSpriteOnCollide = true; //Disable the sprite?
-    public bool loadNextIndex = true; //Just loads the next on in the list
-    public int sceneIndex; //Dndex number of the next scene
-    public float loadDelay; //Delay before loading next lvl
-    SpriteRenderer sp;
 
     public string nextScene = null; // name of the nextScene loaded
     // Start is called before the first frame update
     void Start()
     {
-        if(disableSpriteOnCollide)
-        {
-            try
-            {
-                sp = this.gameObject.GetComponent<SpriteRenderer>();
-            } catch
-            {
-                Debug.Log("ERROR: CAN'T FIND SPRITE RENDERER");
-            }
-        }
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    // Update is called once per frame
+    void Update()
     {
-        if(other.gameObject.tag == "Player")
-        {
-            DisableSprite();
-            if(loadNextIndex) GameManager.Instance.LoadNextLevel(loadDelay);
-            else GameManager.Instance.LoadNextLevel(loadDelay, sceneIndex);
-        }
+        
     }
 
-    void DisableSprite()
-    {
-        if(disableSpriteOnCollide)
     private void OnTriggerEnter2D(Collider2D other) {
         // if no alternative Scene is typed in load standard next scene
         if(string.IsNullOrEmpty(nextScene))
         {
-            sp.enabled = false;
             if(other.gameObject.transform.parent.tag == "Player")
             {
                 Timer.timerIsRunning = false;
