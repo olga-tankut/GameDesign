7f97af26bdcbbfa1758d13db746f6808ab63192e
Korpsian <korbinian.maag@gmail.com>
2021-12-15 15:11:30 +0100

    LoadTrigger code copy

--- Assets/Scripts/LevelLoadTrigger.cs
+++ Assets/Scripts/LevelLoadTrigger.cs
@@ -5,40 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoadTrigger : MonoBehaviour
 {
+    public bool disableSpriteOnCollide = true; //Disable the sprite?
+    public bool loadNextIndex = true; //Just loads the next on in the list
+    public int sceneIndex; //Dndex number of the next scene
+    public float loadDelay; //Delay before loading next lvl
+    SpriteRenderer sp;
 
-    public string nextScene = null; // name of the nextScene loaded
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        // if no alternative Scene is typed in load standard next scene
-        if(string.IsNullOrEmpty(nextScene))
+        if (disableSpriteOnCollide)
         {
-            if(other.gameObject.transform.parent.tag == "Player")
+            try
             {
-                Timer.timerIsRunning = false;
-                GameManager.Instance.LoadNextLevel();
+                sp = this.gameObject.GetComponent<SpriteRenderer>();
+            }
+            catch
+            {
+                Debug.Log("ERROR: CAN'T FIND SPRITE RENDERER");
             }
         }
-        else
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
         {
-            Timer.timerIsRunning = false;
-            LoadNextLevel();
+            DisableSprite();
+            if (loadNextIndex) GameManager.Instance.LoadNextLevel(loadDelay);
+            else GameManager.Instance.LoadNextLevel(loadDelay, sceneIndex);
         }
     }
 
-    public void LoadNextLevel()
+    void DisableSprite()
     {
-        Debug.Log("you have overriden the next scene in the Build Load order with a custom one");
-        SceneManager.LoadScene(nextScene);
+        if (disableSpriteOnCollide)
+        {
+            sp.enabled = false;
+        }
     }
+
 }
